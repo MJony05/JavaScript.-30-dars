7465fa8a149937813f9b539b42c5a8c00b6267b9
Jonibek <94430009+MJony05@users.noreply.github.com>
2022-05-18 23:37:25 +0500

    FINISHED

--- js/script.js
+++ js/script.js
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
     addForm = document.querySelector(".add"),
     addInput = document.querySelector(".adding__input"),
     checkbox = addForm.querySelector("[type='checkbox']");
-
   const news = [
     "FOOTBALL",
     "BASKETBALL",
@@ -33,62 +32,97 @@ document.addEventListener("DOMContentLoaded", () => {
     "BOX",
     "AMERICAN FOOTBAL IN EU...",
   ];
-
-  const sortArr = (arr) => {
-    arr.sort();
-  };
-
-  addForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let newFilm = addInput.value;
-    const favourite = checkbox.checked;
-    console.log(favourite);
-
-    if (newFilm) {
-      if (newFilm.length > 21) {
-        newFilm = `${newFilm.substring(0, 21)}...`;
-      }
-      if (favourite) {
-        console.log("sevimli yangilik qo'shilmoqda");
-      }
-      news.push(newFilm);
-      sortArr(news);
-      createNewsList(news, listNews);
-    }
-
-    addInput.value = "";
-  });
-
-  btnNews.remove();
-
-  newGenre.textContent = "uznews";
-  newGenre.style.color = "crimson";
-
-  bg.style.backgroundImage = "url(img/2.jpg)";
-
-  function createNewsList(newsList, parent) {
+  const render = function (arr, parent) {
+    news.sort();
     parent.innerHTML = "";
-    sortArr(news);
-
-    newsList.forEach((itemNews, index) => {
-      parent.innerHTML += `
-      <li class="promo__interactive-item"> 
-        ${index + 1} ${itemNews}
-        <div class="delete"></div>
-      </li>
-    `;
+    arr.forEach((el, i) => {
+      parent.insertAdjacentHTML(
+        "beforeend",
+        `<li class="promo__interactive-item">
+                ${i + 1 + ". " + el}
+                <div class="delete"></div>
+              </li>`
+      );
     });
-
     document.querySelectorAll(".delete").forEach((btn, i) => {
       btn.addEventListener("click", () => {
         btn.parentElement.remove();
         news.splice(i, 1);
-
-        createNewsList(newsList, parent);
+        render(arr, parent);
       });
     });
-  }
+  };
+  render(news, listNews);
+  addForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let newFilm = addInput.value;
+    if (newFilm) {
+      if (newFilm.length > 21) {
+        newFilm = `${newFilm.substring(0, 21)}...`;
+        console.log(newFilm);
+      }
+    }
+    news.push(newFilm.toUpperCase());
+    news.sort();
+    addInput.value = "";
+    render(news, listNews);
+  });
 
-  createNewsList(news, listNews);
+  //   const sortArr = (arr) => {
+  //     arr.sort();
+  //   };
+
+  //   addForm.addEventListener("submit", (event) => {
+  //     event.preventDefault();
+
+  //     let newFilm = addInput.value;
+  //     const favourite = checkbox.checked;
+  //     console.log(favourite);
+
+  //     if (newFilm) {
+  //       if (newFilm.length > 21) {
+  //         newFilm = `${newFilm.substring(0, 21)}...`;
+  //       }
+  //       if (favourite) {
+  //         console.log("sevimli yangilik qo'shilmoqda");
+  //       }
+  //       news.push(newFilm);
+  //       sortArr(news);
+  //       createNewsList(news, listNews);
+  //     }
+
+  //     addInput.value = "";
+  //   });
+
+  //   btnNews.remove();
+
+  //   newGenre.textContent = "uznews";
+  //   newGenre.style.color = "crimson";
+
+  //   bg.style.backgroundImage = "url(img/2.jpg)";
+
+  //   function createNewsList(newsList, parent) {
+  //     parent.innerHTML = "";
+  //     sortArr(news);
+
+  //     newsList.forEach((itemNews, index) => {
+  //       parent.innerHTML += `
+  //       <li class="promo__interactive-item">
+  //         ${index + 1} ${itemNews}
+  //         <div class="delete"></div>
+  //       </li>
+  //     `;
+  //     });
+
+  //     document.querySelectorAll(".delete").forEach((btn, i) => {
+  //       btn.addEventListener("click", () => {
+  //         btn.parentElement.remove();
+  //         news.splice(i, 1);
+
+  //         createNewsList(newsList, parent);
+  //       });
+  //     });
+  //   }
+
+  //   createNewsList(news, listNews);
 });
